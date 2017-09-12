0d0686025be79ad871b6bbe5c66e284efd854302
ouyang <Administrator@admin.wanfangdata.com.cn>
2017-09-12 15:47:59 +0800

    用户拦截器中去除cookie

--- src/main/java/com/interceptor/UserInterceptor.java
+++ src/main/java/com/interceptor/UserInterceptor.java
@@ -1,12 +1,12 @@
 package com.interceptor;
 
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;  
-import javax.servlet.http.HttpServletResponse;  
-import javax.servlet.http.HttpSession;  
-  
-import org.springframework.web.servlet.HandlerInterceptor;  
-import org.springframework.web.servlet.ModelAndView;  
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.utils.CookieUtil;
 
 /**
  * 拦截器
@@ -16,48 +16,26 @@ import org.springframework.web.servlet.ModelAndView;
  */
 public class UserInterceptor implements HandlerInterceptor {
 
-	private static final String LOGIN_URL = "/user/toLogin.do";
-	private static final String HOMEPAGE_URL = "/user/toIndex.do";
 	
 	//完成后
 	@Override
 	public void afterCompletion(HttpServletRequest arg0,
 			HttpServletResponse arg1, Object arg2, Exception arg3)
-			throws Exception {
-		// TODO Auto-generated method stub
-		
-	}
+			throws Exception {}
 
 	//后处理
 	@Override
 	public void postHandle(HttpServletRequest arg0, HttpServletResponse arg1,
-			Object arg2, ModelAndView arg3) throws Exception {
-		// TODO Auto-generated method stub
-		
-	}
+			Object arg2, ModelAndView arg3) throws Exception {}
 
 	//预处理
 	@Override
 	public boolean preHandle(HttpServletRequest req, HttpServletResponse res, Object handler) throws Exception {  
-		HttpSession session = req.getSession(true);
-        Object obj = session.getAttribute("wfAdmin");
-        //检查cookie
-		String castgc = null;
-		Cookie[] cookies = req.getCookies();
-		if(cookies!=null && cookies.length>0){
-			for(Cookie ck : cookies){
-				if(ck.getName().equals("CASTGC")){							
-					castgc = ck.getValue();
-					break;
-				}
-			}
+		if (CookieUtil.isLogin(req)) {
+			res.sendRedirect(req.getContextPath() + CookieUtil.LOGIN_URL);
+			return false;
 		}
-        
-        if(obj == null || "".equals(obj.toString()) || castgc == null){
-        	res.sendRedirect(req.getContextPath()+LOGIN_URL);
-        	return false;
-        }  
-        return true;
+		return true;
 	}
 	
 } 
