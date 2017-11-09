60f960c86669c79772523fab03c182ae4fb2fea2
ouyang <Administrator@admin.wanfangdata.com.cn>
2017-11-09 17:18:39 +0800

    机构名称和机构名称简称不允许输入汉字

--- src/main/webapp/page/usermanager/js/public.js
+++ src/main/webapp/page/usermanager/js/public.js
@@ -88,17 +88,17 @@ function validStandard(count,i){
 		var startTime=$("#limitedParcelStarttime_"+count+"_"+i).val();
 		var endTime=$("#limitedParcelEndtime_"+count+"_"+i).val();
 		if(startTime==null||startTime==""){
-			layer.msg("开始时间不能为空",{icon: 2,time: 20000});
+			layer.msg("开始时间不能为空",{icon: 2,time: 2000});
 			return false;
 		}
 		if(endTime==null||endTime==""){
-			layer.msg("结束时间不能为空",{icon: 2,time: 20000});
+			layer.msg("结束时间不能为空",{icon: 2,time: 2000});
 			return false;
 		}
 		var d1 = new Date(startTime.replace(/\-/g, "\/"));
 		var d2 = new Date(endTime.replace(/\-/g, "\/"));
 		if(d1>d2){
-			layer.msg("开始时间不能大于结束时间",{icon: 2,time: 20000});
+			layer.msg("开始时间不能大于结束时间",{icon: 2,time: 2000});
 			return false;
 		}
 	}
@@ -108,35 +108,35 @@ function validStandard(count,i){
 		var orgName=$("#orgName_"+count+"_"+i).val();
 		var companySimp=$("#companySimp_"+count+"_"+i).val();
 		if(company==null||company==""){
-			layer.msg("单位名称不能为空",{icon: 2,time: 20000});
+			layer.msg("单位名称不能为空",{icon: 2,time: 2000});
 			return false;
 		}
 		if(orgName==null||orgName==""){
-			layer.msg(" 机构名称不能为空",{icon: 2,time: 20000});
+			layer.msg(" 机构名称不能为空",{icon: 2,time: 2000});
 			return false;
 		}else{
 			if(/.*[\u4e00-\u9fa5]+.*$/.test(orgName)){ 
-				layer.msg(" 机构名称不能包含汉字",{icon: 2,time: 20000});
+				layer.msg(" 机构名称不能包含汉字",{icon: 2,time: 2000});
 				return false; 
 			}
 		}
 		if(companySimp==null||companySimp==""){
-			layer.msg("机构单位简称不能为空",{icon: 2,time: 20000});
+			layer.msg("机构单位简称不能为空",{icon: 2,time: 2000});
 			return false;
 		}else{
 			if(companySimp.length<5 || companySimp.length>30){
-				layer.msg("机构单位简称的长度必须在5到30之间",{icon: 2,time: 20000});
+				layer.msg("机构单位简称的长度必须在5到30之间",{icon: 2,time: 2000});
 				return false;
 			}
 			if(/.*[\u4e00-\u9fa5]+.*$/.test(companySimp)){ 
-				layer.msg(" 机构名称不能包含汉字",{icon: 2,time: 20000});
+				layer.msg(" 机构名称不能包含汉字",{icon: 2,time: 2000});
 				return false; 
 			}
 		}
 	} else if (isBK) {// 2、包库
 		var readingPrint=$("#readingPrint_"+count+"_"+i).val();
 		if(readingPrint==null||readingPrint==""){
-			layer.msg("版权阅读打印不能为空",{icon: 2,time: 20000});
+			layer.msg("版权阅读打印不能为空",{icon: 2,time: 2000});
 			return false;
 		}
 	}
@@ -144,7 +144,7 @@ function validStandard(count,i){
 	if (isZJ || isBK) {
 		var fullIpRange=$("#fullIpRange_"+count+"_"+i).val();
 		if(fullIpRange==null||fullIpRange==""){
-			layer.msg("ip段必须填写",{icon: 2,time: 20000});
+			layer.msg("ip段必须填写",{icon: 2,time: 2000});
 			return false;
 		}
 	}
@@ -524,8 +524,10 @@ function createDetail(count,i,resourceid,type){
 		text += '<div class="form-group input_block"><label><b>*</b>限定时间：</label><input class="Wdate" name="rdlist['+count+'].rldto['+i+'].limitedParcelStarttime" id="limitedParcelStarttime_'+count+'_'+i+'" onclick="WdatePicker()" type="text">';
 		text += '<span class="to">至</span><input class="Wdate" name="rdlist['+count+'].rldto['+i+'].limitedParcelEndtime" id="limitedParcelEndtime_'+count+'_'+i+'" onclick="WdatePicker()" type="text"></div>';
 		text += '<div id="isZJ"><div class="form-group input_block"><label><b>*</b>单位名称：</label><input type="text" class="form-control input_width" name="rdlist['+count+'].rldto['+i+'].company" id="company_'+count+'_'+i+'"></div>';
-		text += '<div class="form-group input_block"><label><b>*</b>机构名称：</label><input type="text" class="form-control input_width" name="rdlist['+count+'].rldto['+i+'].orgName" id="orgName_'+count+'_'+i+'" onblur="checkOrg('+count+','+i+')">（若账号为中文，则填写全拼）</div>';
-		text += '<div class="form-group input_block"><label><b>*</b>机构单位简称：</label><input type="text" class="form-control input_width" name="rdlist['+count+'].rldto['+i+'].companySimp" id="companySimp_'+count+'_'+i+'" onblur="checkOrg('+count+','+i+')"></div></div>';
+		text += '<div class="form-group input_block"><label><b>*</b>机构名称：</label><input type="text" class="form-control input_width" name="rdlist['+count+'].rldto['+i+'].orgName" id="orgName_'+count+'_'+i+'"';
+		text += ' onkeyup="this.value=this.value.replace(/[^a-zA-Z0-9_]/g,\'\')" onafterpaste="this.value=this.value.replace(/[^a-zA-Z0-9_]/g,\'\')" onblur="checkOrg('+count+','+i+')">（若账号为中文，则填写全拼）</div>';
+		text += '<div class="form-group input_block"><label><b>*</b>机构单位简称：</label><input type="text" class="form-control input_width" name="rdlist['+count+'].rldto['+i+'].companySimp" id="companySimp_'+count+'_'+i+'"';
+		text += ' onkeyup="this.value=this.value.replace(/[^a-zA-Z0-9_]/g,\'\')" onafterpaste="this.value=this.value.replace(/[^a-zA-Z0-9_]/g,\'\')"  onblur="checkOrg('+count+','+i+')"></div></div>';
 		text += '<div id="isBK"><div class="form-group input_block"><label><b>*</b>版权阅读打印：</label><select class="form-control input_width" name="rdlist['+count+'].rldto['+i+'].readingPrint" id="readingPrint_'+count+'_'+i+'">';
 		text += '<option value="0" checked>授权阅读打印</option><option value="1">授权阅读</option><option value="2">授权打印</option><option value="3">未阅读</option></select></div>';
 		text += '<div class="form-group input_block"><label>&nbsp;&nbsp;在线用户数：</label><input type="text" class="form-control input_width" name="rdlist['+count+'].rldto['+i+'].onlineVisitor" id="onlineVisitor_'+count+'_'+i+'" value="-1"><span>-1表示不限制</span><br></div>';
