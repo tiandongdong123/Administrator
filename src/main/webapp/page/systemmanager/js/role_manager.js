f860e86b1712c71dd7ec96573b8e89387357be2b
wensj <wensijin@wensijin>
2017-11-10 15:03:39 +0800

    解决【系统管理】角色管理，添加角色时，角色描述长度没控制，添加成功后角色列表中角色描述列与权限列信息重叠的问题。

--- src/main/webapp/page/systemmanager/js/role_manager.js
+++ src/main/webapp/page/systemmanager/js/role_manager.js
@@ -12,7 +12,7 @@ function rolepage(curr){
     	html="";
     for(var i =0;res.pageRow[i];i++){
     	id = 10*(curr-1)+i+1;
-    	html+="<tr><td>"+id+"</td><td>"+res.pageRow[i].deptName+"</td><td>"+res.pageRow[i].roleName+"</td><td>"+res.pageRow[i].description+"</td><td style=\"word-wrap:break-word;\" width=\"160px;\">"+res.pageRow[i].purview+"</td><td><button type='button' class='btn btn-primary' onclick=\"updaterole('"+res.pageRow[i].roleId+"')\">修改</button>" +
+    	html+="<tr><td>"+id+"</td><td style=\"word-wrap:break-word;\" width=\"160px;\">"+res.pageRow[i].deptName+"</td><td style=\"word-wrap:break-word;\" width=\"160px;\">"+res.pageRow[i].roleName+"</td><td>"+res.pageRow[i].description+"</td><td style=\"word-wrap:break-word;\" width=\"160px;\">"+res.pageRow[i].purview+"</td><td><button type='button' class='btn btn-primary' onclick=\"updaterole('"+res.pageRow[i].roleId+"')\">修改</button>" +
     			"<button type='button' class='btn btn-primary' onclick=\"deleterole('"+res.pageRow[i].roleId+"')\">删除</button></td>" +
     					"</tr>";   	
     }
