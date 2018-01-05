34f737390d3596b50d6d074abc085f00cdcbd8c2
wensj <wensijin@wensijin>
2018-01-05 14:48:08 +0800

    修改定时器执行频率。

--- src/main/java/com/job/HotWordSetRedisJob.java
+++ src/main/java/com/job/HotWordSetRedisJob.java
@@ -35,7 +35,7 @@ public class HotWordSetRedisJob {
 	private static Logger log = Logger.getLogger(HotWordSetRedisJob.class);
 	private SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 	
-	@Scheduled(cron = "0 0/1 * * * ?")
+	@Scheduled(cron = "0 0 0/1 * * ?")
 	public void execHotWordSetRedis() {
 		try{
 			log.info("开始热搜词redis发布");
