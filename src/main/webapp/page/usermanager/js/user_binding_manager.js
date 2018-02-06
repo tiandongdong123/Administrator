89b314a7a31828548c0cff70e6b06c4b724c6d17
zhangrui <rui.912>
2018-02-06 16:53:59 +0800

    点击下一页后每页显示问题

--- src/main/webapp/page/usermanager/js/user_binding_manager.js
+++ src/main/webapp/page/usermanager/js/user_binding_manager.js
@@ -1,4 +1,5 @@
 $(function(){
+    var page_show=20;
     redq();
     if($(".parameter").val()!=""){
        $("#userId").val($(".parameter").val());
@@ -190,7 +191,7 @@ $(function(){
                 $(".mechanism_id").css("border-color","#dd4b39");
                 $(".bind_numm").css("color","#dd4b39");
                 $(".wrongm").css("background","url(../img/f.png)");
-                $(".wrongm").css("margin-left","6px");
+                $(".wrongm").css("margin-left","9px");
                 $(".mistakenm").css("display","inline");
                 $(".wrongm").css("display","inline");
                 $(".mistakenm").text("机构ID不能为空");
@@ -442,42 +443,48 @@ function timeStamp2String(time){
     var getPager = function (url, $container) {
         $.get(url, function (html) {
             $container.replaceWith(html);
+            $('.sync-html').html(html);
             redq();
         });
     };
     //page-a异步跳转
     $(document).on('click', '.sync-html .page_bind a', function () {
+        var evey = $(".evey-page").val();
         $('.sync-html').empty('');
         var href = $(this).attr('href');
         $(this).removeAttr('href');
         $.get(href, function (html) {
             $('.sync-html').html(html);
+            $(".evey-page").val(evey);
             redq();
         });
 
     });
     //page-form异步跳转
     $(document).on('submit', '.sync-html .page_bind form', function () {
+        var evey = $(".evey-page").val();
         var action = $(this).attr('action');
         var inputPage = parseInt($(this).find('.laypage_skip').val());
         var allPage = $(this).attr('data-all');
         if (inputPage > 0 && inputPage <= allPage) {
             var href = action + inputPage;
             getPager(href, $(this).closest('.sync-html'));
+            $('.sync-html').html(html);
             redq();
         } else {
             alert('请输入正确页码');
         }
-
         return false;
     });
     //page-form同步跳转
     $(document).on('submit', '.no-sync .page_bind form', function () {
+        var evey = $(".evey-page").val();
         var action = $(this).attr('action');
         var inputPage = parseInt($(this).find('.laypage_skip').val());
         var allPage = $(this).attr('data-all');
         if (inputPage > 0 && inputPage <= allPage) {
             window.location.href = action + encodeURIComponent(inputPage);
+            $('.sync-html').html(html);
             redq();
         } else {
             alert('请输入正确页码');
