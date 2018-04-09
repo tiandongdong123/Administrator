ebeac449893cd7856ccd8d1ba086e0eea202e2d8
ouyang <Administrator@admin.wanfangdata.com.cn>
2018-04-09 11:14:34 +0800

    删除多余的类

--- src/main/java/com/wf/controller/SolrManagerController.java
+++ src/main/java/com/wf/controller/SolrManagerController.java
@@ -1,8 +1,6 @@
 package com.wf.controller;
 
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
