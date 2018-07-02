15e64e6959ea83f71bc1cb393deadcb8e775fca2
ouyang <Administrator@admin.wanfangdata.com.cn>
2018-07-02 15:53:55 +0800

    添加权限的查询方法

--- src/main/java/com/wf/dao/WfksAccountidMappingMapper.java
+++ src/main/java/com/wf/dao/WfksAccountidMappingMapper.java
@@ -11,4 +11,5 @@ public interface WfksAccountidMappingMapper {
     WfksAccountidMapping[] getWfksAccountidByIdKey(@Param("idKey")String idKey);
     WfksAccountidMapping[] getWfksAccountid(@Param("idKey")String idKey, @Param("relatedidAccounttype")String type);
     WfksAccountidMapping[] getWfksAccountidLimit(@Param("idKey")String idKey, @Param("idAccounttype")String type);
+    WfksAccountidMapping[]  getWfksAccountidByRelatedidKey(@Param("relatedidKey")String relatedidKey);
 }
