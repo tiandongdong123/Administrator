7422f12067df93cd2bcab83b227c0b4102763981
刘帅亚 <>
2018-07-11 17:46:29 +0800

    个人绑定机构类添加属性

--- src/main/java/com/wf/bean/BindAuthorityModel.java
+++ src/main/java/com/wf/bean/BindAuthorityModel.java
@@ -4,6 +4,10 @@ package com.wf.bean;
  * Created by syl on 2018/1/12.
  */
 
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
+
 /**
  * 个人绑定机构参数类
  */
@@ -12,7 +16,13 @@ public class BindAuthorityModel {
     private Boolean openState;
     //机构账号id
     private String userId;
-    //绑定模式
+    //开通时限起始时间
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private Date openBindStart;
+    //开通时限结束时间
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private Date openBindEnd;
+    //绑定模式   机构个人同时登录--0  机构登录--1  线下扫描--2
     private Integer bindType;
     //绑定个人上限
     private Integer bindLimit;
@@ -22,6 +32,8 @@ public class BindAuthorityModel {
     private Integer downloadLimit;
     //绑定个人继承权限
     private String bindAuthority;
+    //邮箱
+    private String email;
 
     public Boolean getOpenState() {
         return openState;
@@ -37,6 +49,22 @@ public class BindAuthorityModel {
 
     public void setUserId(String userId) {
         this.userId = userId;
+    }
+
+    public Date getOpenBindStart() {
+        return openBindStart;
+    }
+
+    public void setOpenBindStart(Date openBindStart) {
+        this.openBindStart = openBindStart;
+    }
+
+    public Date getOpenBindEnd() {
+        return openBindEnd;
+    }
+
+    public void setOpenBindEnd(Date openBindEnd) {
+        this.openBindEnd = openBindEnd;
     }
 
     public Integer getBindType() {
@@ -79,16 +107,27 @@ public class BindAuthorityModel {
         this.bindAuthority = bindAuthority;
     }
 
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     @Override
     public String toString() {
         return "BindAuthorityModel{" +
                 "openState=" + openState +
                 ", userId='" + userId + '\'' +
+                ", openBindStart=" + openBindStart +
+                ", openBindEnd=" + openBindEnd +
                 ", bindType=" + bindType +
                 ", bindLimit=" + bindLimit +
                 ", bindValidity=" + bindValidity +
                 ", downloadLimit=" + downloadLimit +
                 ", bindAuthority='" + bindAuthority + '\'' +
+                ", email='" + email + '\'' +
                 '}';
     }
 }
