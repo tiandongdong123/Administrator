9988a507c8d1cade25e2f27ccae066afad931fa2
ouyang <Administrator@admin.wanfangdata.com.cn>
2018-08-16 15:15:45 +0800

    金额录入范围规则，按最大值计算

--- src/main/webapp/page/usermanager/js/public.js
+++ src/main/webapp/page/usermanager/js/public.js
@@ -650,10 +650,10 @@ function selectProject(obj,flag,checked){
 			text += '<span><b>*</b>时限</span><input type="text" class="Wdate" value="'+getData()+'" name="rdlist['+count+'].validityStarttime" id="'+projectid+'_st" onclick="WdatePicker({maxDate:\'#F{$dp.$D('+projectid+'_et)}\'})"/>';
 			text += '<span class="to">至</span><input type="text" class="Wdate" name="rdlist['+count+'].validityEndtime" id="'+projectid+'_et" onclick="WdatePicker({minDate:\'#F{$dp.$D('+projectid+'_st)}\'})"></div>';
 			if($(obj).val()!="time"&&$(obj).val()!="count"&&flag!='2'){
-				text += '<div class="time_input time_money"><span><b>*</b>金额</span><input type="text" name="rdlist['+count+'].totalMoney" onkeyup="checkMoney(this);" onafterpaste="checkMoney(this);" maxlength="9"></div>';
+				text += '<div class="time_input time_money"><span><b>*</b>金额</span><input type="text" name="rdlist['+count+'].totalMoney" onkeyup="checkMoney(this);" onafterpaste="checkMoney(this);"></div>';
 			}
 			if($(obj).val()!="time"&&$(obj).val()!="balance"&&flag!='2'){
-				text += '<div class="time_input time_money"><span><b>*</b>次数</span><input type="text" name="rdlist['+count+'].purchaseNumber" onkeyup="checkNum(this);" onafterpaste="checkNum(this);" maxlength="9"></div>';
+				text += '<div class="time_input time_money"><span><b>*</b>次数</span><input type="text" name="rdlist['+count+'].purchaseNumber" onkeyup="checkNum(this);" onafterpaste="checkNum(this);"></div>';
 			}
 			if(projectid=='HistoryCheck'){
 				text += '<p><div class="time_input"><span><b>*</b>查看交易信息</span> ';
@@ -1956,22 +1956,19 @@ function deleteIP(){
 	$("#ipSegment").val(ipHtml);
 }
 //验证次数
-
+var maxData=99999999;
 function checkNum(obj){
 	var val=obj.value;
 	var flag=false;
-	var len=8;
 	if(val.substr(0,1)=="-"){
-		len=9;
 		flag=true;
 	}
 	val= val.replace(/[^\d]/g,""); //清除"数字"和"."以外的字符
 	val = val.replace(/^\./g,""); //验证第一个字符是数字
 	val = val.replace(/\.{2,}/g,"."); //只保留第一个, 清除多余的
 	val = val.replace(".","$#$").replace(/\./g,"").replace("$#$",".");
-	val = val.replace(/^(\-)*(\d+)\.(\d\d).*$/,'$1$2.$3'); //只能输入两个小数
-	if(val.length>len){
-		val=val.substring(0, len);
+	if(parseFloat(val)>maxData){
+		val=val.substring(0,8);
 	}
 	obj.value=(flag?"-":"")+val;
 }
@@ -1979,9 +1976,7 @@ function checkNum(obj){
 function checkMoney(obj){
 	var val=obj.value;
 	var flag=false;
-	var len=8;
 	if(val.substr(0,1)=="-"){
-		len=9;
 		flag=true;
 	}
 	val= val.replace(/[^\d.]/g,""); //清除"数字"和"."以外的字符
@@ -1989,8 +1984,8 @@ function checkMoney(obj){
 	val = val.replace(/\.{2,}/g,"."); //只保留第一个, 清除多余的
 	val = val.replace(".","$#$").replace(/\./g,"").replace("$#$",".");
 	val = val.replace(/^(\-)*(\d+)\.(\d\d).*$/,'$1$2.$3'); //只能输入两个小数
-	if(val.length>len){
-		val=val.substring(0, len);
+	if(parseFloat(val)>maxData){
+		val=val.substring(0,8);
 	}
 	obj.value=(flag?"-":"")+val;
 }
