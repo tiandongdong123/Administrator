c4e780edb3b2366bbd91d8f7d0f9b96002ed7432
wensj <123456>
2018-09-28 11:03:30 +0800

    新增配置文件类.

---
+++ src/main/java/com/wanfangdata/grpcchannel/GrpcServer.java
@@ -0,0 +1,77 @@
+package com.wanfangdata.grpcchannel;
+
+import io.grpc.ManagedChannel;
+import io.grpc.netty.NegotiationType;
+import io.grpc.netty.NettyChannelBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Properties;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+public class GrpcServer {
+
+    private static final Logger log = LogManager.getLogger(GrpcServer.class);
+
+    private static ManagedChannel originChannel;
+
+    private static final String GRPCSERVER_CONFIG="grpcServer.properties";
+
+    //访问地址
+    private static String host;
+    //访问端口号
+    private static Integer port;
+
+
+    private GrpcServer() {
+        Properties pro = new Properties();
+        InputStream in = Thread.currentThread().getContextClassLoader().getResourceAsStream(GRPCSERVER_CONFIG);
+        try {
+            pro.load(new InputStreamReader(in,"UTF-8"));
+            GrpcServer.host = pro.getProperty("grpc_host");
+            GrpcServer.port = Integer.valueOf(pro.getProperty("grpc_port"));
+            in.close();
+        } catch (Exception var8) {
+            log.error("无法加载配置文件" + GRPCSERVER_CONFIG);
+        } finally {
+            try {
+                in.close();
+            } catch (IOException e) {
+                log.error("关闭输入流失败");
+            }
+        }
+    }
+
+    public static ManagedChannel getInstance() {
+
+        if (originChannel == null) {
+            synchronized (GrpcServer.class) {
+                if (null == originChannel) {
+                    originChannel = NettyChannelBuilder.forAddress(host, port)
+                            .negotiationType(NegotiationType.PLAINTEXT)
+                            .build();
+                }
+            }
+        }
+        return originChannel;
+    }
+
+    public static String getHost() {
+        return host;
+    }
+
+    public static void setHost(String host) {
+    	GrpcServer.host = host;
+    }
+
+    public static Integer getPort() {
+        return port;
+    }
+
+    public static void setPort(Integer port) {
+    	GrpcServer.port = port;
+    }
+}
