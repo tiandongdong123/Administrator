4bb2997f3694ff4619386a28d643debe6fe6ab78
liux <769859439@qq.com>
2019-01-21 20:36:59 +0800

    个人现金统计页面

---
+++ src/main/java/com/wf/controller/TransactionStatisticsController.java
@@ -0,0 +1,25 @@
+package com.wf.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * 交易统计
+ */
+@Controller
+@RequestMapping("transactionStatistics")
+public class TransactionStatisticsController {
+
+    /**
+     * 个人现金交易统计
+     *
+     * @return
+     */
+    @RequestMapping("personStatistics")
+    public ModelAndView personStatistics() {
+        ModelAndView view = new ModelAndView();
+        view.setViewName("/page/statistics/person_statistics");
+        return view;
+    }
+}
